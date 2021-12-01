73a8300729521a32edcc7ab149f8df6f4f144b54
hankyu Jang <hankyujang@hankyuui-MacBookPro.local>
2021-12-01 19:30:28 +0900

    init

---
+++ src/test/java/org/hankyu/myhome/MyhomeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hankyu.myhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyhomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
