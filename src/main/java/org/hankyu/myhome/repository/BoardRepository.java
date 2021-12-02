72f214f7b7e826e128d7bfec778f7a913aaf0521
hankyujang <finebears@naver.com>
2021-12-02 17:03:39 +0900

    JPA이용해서 마리아DB의 데이터 조작할 수 있는 컨트롤러 생성 PostMan을 이용해서 http요청을 통해 CURD 데이터 조작하기

--- src/main/java/org/hankyu/myhome/repository/BoardRepository.java
+++ src/main/java/org/hankyu/myhome/repository/BoardRepository.java
@@ -3,6 +3,10 @@ package org.hankyu.myhome.repository;
 import org.hankyu.myhome.model.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
+    List<Board> findByTitle(String title);
+    List<Board> findByTitleOrContent(String title, String content);
 }
